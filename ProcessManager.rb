fcd5920637f994013e01808a0a2b6a06ef07214d
Pablo García <pablogarciabenayas@users.noreply.github.com>
2017-11-21 22:42:05 +0100

    Update ProcessManager.rb

--- ProcessManager.rb
+++ ProcessManager.rb
@@ -3,16 +3,11 @@ require 'Process_'
 class ProcessManager
   def initialize
     @processList = []
-    @processGroupedByName = Hash.new()
   end
 
   def addProcess(p)
     @processList.push(p)
   end
-
-  #  def each_process()
-  #    @processList.each() {|p| puts p}
-  #  end
 
   def each_process()
     @processList.each do |p|
@@ -24,18 +19,21 @@ class ProcessManager
     childProcessList = @processList.find_all() {|p| p.parentId == parent.id}
     childProcessList.each do |p|
       yield p
+      each_child_process(p) do |other_p|
+        yield other_p
+      end 
     end
   end
 
   def top_memory_consumers()
     topMemoryConsumers = @processList.sort()
     return topMemoryConsumers.reverse()[0,10]
-    #    return topMemoryConsumers.reverse()[0..9]
   end
 
   def memory()
-    @processList.each {|k| addToSet(@processGroupedByName,k.name,k.memory_size)}
-    return @processGroupedByName
+    processGroupedByName = Hash.new()
+    @processList.each {|k| addToSet(processGroupedByName,k.name,k.memory_size)}
+    return processGroupedByName
   end
 
   def addToSet(set,key,value)
